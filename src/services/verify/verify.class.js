75d5d16487b33fc21a1191d2139294a68b3bec26
tanedward21 <tanedward2101@gmail.com>
2022-11-28 11:05:24 +0700

    change code

--- src/services/verify/verify.class.js
+++ src/services/verify/verify.class.js
@@ -7,21 +7,30 @@ exports.Verify = class Verify {
 
   async find(params) {
     var filter
- 
+
     const dbQR = this.app.get('dbqr');
     var data
 
     if (params.query.code) {
 
       data = await dbQR('qrdb').select('*').where('code', params.query.code)
-      await dbQR('qrdb').increment('scanned').where('code', params.query.code)
+
       if (data[0].scanned == 0) {
         await dbQR('qrdb').update('first_verify_date', new Date()).where('code', params.query.code)
-      } else { await dbQR('qrdb').update('last_verify_date', new Date()).where('code', params.query.code) }
-    } else { data = "Null" }
+      }
+      else {
+        await dbQR('qrdb').update('last_verify_date', new Date()).where('code', params.query.code)
+      }
+    }
+    else {
+      data = "Null"
+    }
 
     if (data.length == 0) {
       data[0] = "Null"
+    }
+    else {
+      await dbQR('qrdb').increment('scanned').where('code', params.query.code)
     }
 
     return data;
