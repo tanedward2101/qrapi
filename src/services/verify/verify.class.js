5277215897970ee4d2a2f296ccc375c8da22ffef
tanedward21 <tanedward2101@gmail.com>
2022-11-29 23:37:54 +0700

    addf

--- src/services/verify/verify.class.js
+++ src/services/verify/verify.class.js
@@ -1,3 +1,5 @@
+const e = require("cors");
+
 /* eslint-disable no-unused-vars */
 exports.Verify = class Verify {
   constructor(options, app) {
@@ -13,24 +15,28 @@ exports.Verify = class Verify {
 
     if (params.query.code) {
 
-      data = await dbQR('qrdb').select('*').where('code', params.query.code).andWhere('active',1)
-
-      if (data[0].scanned == 0) {
-        await dbQR('qrdb').update('first_verify_date', new Date()).where('code', params.query.code).andWhere('active',1)
+      data = await dbQR('qrdb').select('*').where('code', params.query.code).andWhere('active', 1)
+      if (data.length != 0) {
+        if (data[0].scanned == 0) {
+          await dbQR('qrdb').update('first_verify_date', new Date()).where('code', params.query.code).andWhere('active', 1)
+        }
+        else {
+          await dbQR('qrdb').update('last_verify_date', new Date()).where('code', params.query.code).andWhere('active', 1)
+        }
       }
       else {
-        await dbQR('qrdb').update('last_verify_date', new Date()).where('code', params.query.code).andWhere('active',1)
+        data[0] = "Null"
       }
     }
     else {
-      data = "Null"
+      data[0] = "Null"
     }
 
     if (data.length == 0) {
       data[0] = "Null"
     }
     else {
-      await dbQR('qrdb').increment('scanned').where('code', params.query.code).andWhere('active',1)
+      await dbQR('qrdb').increment('scanned').where('code', params.query.code).andWhere('active', 1)
     }
 
     return data;
