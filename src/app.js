38ee80c167799e3215c58ff3c20836df4d904f94
tanedward21 <tanedward2101@gmail.com>
2023-09-14 15:17:53 +0700

    limit

--- src/app.js
+++ src/app.js
@@ -30,8 +30,8 @@ app.use(helmet({
 }));
 app.use(cors());
 app.use(compress());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({limit: '100mb'}));
+app.use(express.urlencoded({ extended: true, limit: '100mb' }));
 app.use(favicon(path.join(app.get('public'), 'favicon.ico')));
 // Host the public folder
 app.use('/', express.static(app.get('public')));
@@ -53,8 +53,8 @@ app.configure(channels);
 // Configure a middleware for 404s and the error handler
 app.use(express.notFound());
 app.use(express.errorHandler({ logger }));
-app.use(express.json({limit: '100mb'}));
-app.use(express.urlencoded({limit: '100mb'}));
+
+
 app.hooks(appHooks);
 
 module.exports = app;
